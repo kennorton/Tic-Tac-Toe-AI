6180410a7aab06f6ab42b4721d5a1aecf7c2371e
MikeAustinn <103225984+MikeAustinn@users.noreply.github.com>
2022-04-20 13:03:59 -0700

    Add files via upload

---
+++ board.py
@@ -0,0 +1,18 @@
+
+def print_tic_tac_toe():
+    print("\n")
+    print("\t     |     |")
+    print("\t     |     |  ")
+    print('\t_____|_____|_____')
+ 
+    print("\t     |     |")
+    print("\t     |     |  ")
+    print('\t_____|_____|_____')
+ 
+    print("\t     |     |")
+ 
+    print("\t     |     |  ")
+    print("\t     |     |")
+    print("\n")
+print_tic_tac_toe()
+
