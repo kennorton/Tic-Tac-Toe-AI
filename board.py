2d0725af11ea46479aeea493a51097e117440c71
Avery Tataryn <100679454+aqtaryn@users.noreply.github.com>
2022-04-21 22:26:12 -0700

    Update board.py

--- board.py
+++ board.py
@@ -2,16 +2,26 @@ from numpy import array, array_equal
 
 def make_board() -> array:
     """ Make a board formatted as a 2d array.
-    Positions can be referenced through board[y][x]
+    TODO: Positions can be referenced through board[y][x]
     """
-    board = [['.' for x in range(3)] for y in range(3)]
+
+    board = [[' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '],
+             ['_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_'],
+             [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '],
+             ['_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_', '|', '_', '_', '_', '_', '_'],
+             [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' ']]
+    
     return board
 
 def print_board(board: array):
     """ prints 2d array with space seperating columns.
     '\\n' seperateing rows, with '\\n' at end of array.
     """
-    print('\n'.join([' '.join([col for col in row])
+    print('\n'.join([''.join([col for col in row])
         for row in board]))
 
 """
