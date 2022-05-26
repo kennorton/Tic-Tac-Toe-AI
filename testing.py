f7be1b93f59a9db7f248e4598ad62147d308d527
Avery Tataryn <100679454+aqtaryn@users.noreply.github.com>
2022-05-26 14:35:25 -0700

    Test Win Check
    Added a test for the win check

--- testing.py
+++ testing.py
@@ -11,5 +11,9 @@ class FunctionTesting(unittest.TestCase):
         new = [1, 0, 0, 0, 0, 0, 0, 0, 0]
         self.assertEqual(f.take_move(old, 1, 1), new)
 
+    def test_win_check(self):
+        x_win = [1, 1, 1, 0, 2, 0, 0, 0, 2]
+        self.assertEqual(f.win_check(x_win), 1)
+
 if __name__ == '__main__':
     unittest.main()
