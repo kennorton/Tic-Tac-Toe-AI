23c25a93d3bba3c58cc2f03360b4bde91814b5a7
kennorton <103226616+kennorton@users.noreply.github.com>
2022-06-01 13:47:16 -0700

    Add Main Menu graphics and navigation function
    Added menuGraphic and menuNav functions.

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-""" A file for most supporting functions 
+""" A file for most supporting functions
     Please add tests for functions added in the testing.py file """
 
 from numpy import array
@@ -18,7 +18,7 @@ def take_move(data: array, player: int, move: int):
     return data
 
 def update():
-    """ Update data for game to use 
+    """ Update data for game to use
     Args: Data to update, Board(s) to update
     Return: Updated Board(s) """
     pass
@@ -26,3 +26,35 @@ def update():
 def win_check():
     # Only needs to be used 5 moves in
     pass
+
+def menuGraphic():
+    """ Print a main menu graphic
+    Args: None
+    Return: Main menu graphic """
+    print()
+    print("                                           WELCOME TO...                                          ")
+    print(" ______   __     ______         ______   ______     ______          ______   ______     ______.   ")
+    print("/\__  _\ /\ \   /\  ___\       /\__  _\ /\  __ \   /\  ___\        /\__  _\ /\  __ \   /\  ___\   ")
+    print("\/_/\ \/ \ \ \  \ \ \____  ---  \/_/\ \/ \ \  __ \  \ \ \____ ---  \/_/\ \/ \ \ \/\ \  \ \  __\.  ")
+    print("   \ \_\  \ \_\  \ \_____\         \ \_\  \ \_\ \_\  \ \_____\        \ \_\  \ \_____\  \ \_____\ ")
+    print("    \/_/   \/_/   \/_____/          \/_/   \/_/\/_/   \/_____/         \/_/   \/_____/   \/_____/ ")
+    print()
+    print("------------------------------------------------------------------------------------------------- ")
+
+def menuNav():
+    """ Ask for user input for main menu navigation 
+    Args: None
+    Return: None """
+    flag = True
+    while flag == True:
+        choice = input("What would you like to do? ")
+        if choice == "play":
+            pass
+            #game()
+            flag = False
+        elif choice == "settings":
+            pass
+            #settings()
+            flag = False
+        else:
+            print("Please choose something from the menu")
